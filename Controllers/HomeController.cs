65aa0fe7e6463293a481a3cf982fe8d48ee26a3f
Roman Dzygun <dzygun-roman@mail.ru>
2022-07-20 11:45:28 +0300

    PartyInvites

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        IActionResult Index() 
+        public IActionResult Index() 
+        {
+            return View();
+        }
+        public ViewResult RsvpForm()
         {
             return View();
         }
