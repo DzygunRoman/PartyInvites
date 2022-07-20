577bf4fd593daed866f66095496e2f1e5900fa2a
Roman Dzygun <dzygun-roman@mail.ru>
2022-07-20 12:53:53 +0300

    PartyInviates

--- Models/GuestResponse.cs
+++ Models/GuestResponse.cs
@@ -5,6 +5,6 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public bool? WillAtend { get; set; }//?-допускает значение null
+        public bool? WillAttend { get; set; }//?-допускает значение null
     }
 }
